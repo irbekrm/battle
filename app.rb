fb5cce838744572323e8e480ceae97b0a0f39603
Irbe Krumina <irbekrm@gmail.com>
2018-04-06 06:55:01 +0100

    pass players names as form params

--- app.rb
+++ app.rb
@@ -3,8 +3,14 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 
   get '/' do
-    "Test infrastructure working!"
+    erb :index
   end
   
+  post '/names' do
+    #@player1 = params['player1']
+    #@player2 = params['player2']
+    erb :player, { locals: { player1: params['player1'], player2: params['player2'] } }
+  end
+
   run! if app_file == $0
 end
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,6 +1,11 @@
 describe 'the landing page', type: :feature  do
-  it 'has the right content' do
-    visit '/'
-    expect(page).to have_content('Test infrastructure working!')
+  feature 'Display players names' do 
+    scenario 'User enters players names' do
+      visit '/'
+      fill_in 'player1', with: 'Anna'
+      fill_in 'player2', with: 'Reinis'
+      click_button 'Submit'
+      expect(page).to have_content 'Anna', 'Reinis'
+    end
   end
 end

fc3fdb5587ad606f535ff2a7dfb5541965b63a24
Irbe Krumina <irbekrm@gmail.com>
2018-04-06 11:38:52 +0100

    attack reduces hit points

--- app.rb
+++ app.rb
@@ -11,16 +11,23 @@ class Battle < Sinatra::Base
   post '/names' do
     session['player1'] = params['player1']
     session['player2'] = params['player2']
+    session['player1_hit_points'] = 100
+    session['player2_hit_points'] = 100
     redirect '/play'
   end
 
 
   get '/play' do
-    @player1_hit_points = 100
-    @player2_hit_points = 100
+    @player1_hit_points = session['player1_hit_points']
+    @player2_hit_points = session['player2_hit_points']
     @player1 = session['player1']
     @player2 = session['player2']
     erb :player
+  end
+
+  post '/attack' do
+    session['player2_hit_points'] -= 10
+    redirect '/play'
   end
 
   run! if app_file == $0
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -12,4 +12,12 @@ describe 'the landing page', type: :feature  do
       expect(page).to have_content '100', count: 2
     end
   end
+
+  feature 'Updates players hit points' do
+    scenario 'Player1 hits once' do
+      sign_in_and_play
+      click_button 'Attack!'
+      expect(page).to have_content '90', '100'
+    end
+  end  
 end
